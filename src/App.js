ef2a3cce71c9446abbaadf2d9ee32c12ef5fbb55
Josh Uvi <joshuauvbiekpahor@gmail.com>
2019-11-21 12:20:07 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+const App = () => {
+    return (
+        <div>
+            <h1>Wow!! react and webpack built from scratch!!!</h1>
+        </div>
+    )
+}
+
+
+export default App;
