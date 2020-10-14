6c26c79210d16dc8d2ed40b89e79cd640b421b27
Josh Uvi <joshuauvbiekpahor@gmail.com>
2020-10-14 14:20:05 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react';
+import './App.css';
 
 const App = () => {
-    return (
-        <div>
-            <h1>Wow!! react and webpack built from scratch!!!</h1>
-        </div>
-    )
-}
-
+  const AppLayout = () => (
+    <>
+      <header>Header - it will contain the nav bar</header>
+      <aside>Sidebar - This is will contain the sidebar menu</aside>
+      <main>
+        <p>
+          Main - Wow it works!! This is starter pack project, with react and
+          webpack built from scratch! Port-env: {process.env.PORT}
+        </p>
+      </main>
+      <footer className='footer'>Footer - Made by Josh Uvi</footer>
+    </>
+  );
+  return <AppLayout />;
+};
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,50 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+const PORT = process.env.PORT || 4000;
+const HOST = process.env.HOST || 'localhost';
 
 module.exports = {
-    entry: './public/entry.js',
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'bundle.js'
+  entry: './public/entry',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new Dotenv(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  devServer: {
+    contentBase: './public',
+    hot: true,
+    open: true,
+    host: HOST,
+    port: PORT,
+    watchOptions: {
+      poll: true,
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
-    ]
-}
+  },
+};
